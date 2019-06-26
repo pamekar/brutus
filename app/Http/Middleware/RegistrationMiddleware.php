99f5ad594e441167ba37ad17d168f6b09ffe3330
GreenWhite <greenwhitedev@gmail.com>
2019-06-26 14:46:09 +0100

    Final Commit
    Changes Were made to:
    App/Http/Kernel
    App/Http/Middleware/RegisrationMiddleware
    routes/web
    resources/views/auth/register.blade.php

---
+++ app/Http/Middleware/RegistrationMiddleware.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+
+class RegistrationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $password = $request->all()['password'];
+
+        $validation = [];
+        if (Str::upper($password[0]) !== $password[0]
+            && Str::upper($password[1]) !== $password[1]
+        ) {
+            $validation["beginsUpper"]
+                = "Your password should begin with capital letters as first digits";
+        }
+
+        if (preg_match("/[a-z]/", $password) === 0) {
+            $validation['containsLower']
+                = "Your Password should consist of lower case letters";
+        }
+
+        if (preg_match_all("/[`'\\~!@# $*%^&()<>,:;{}\|]/", $password) < 2) {
+            $validation['containsSymbols']
+                = "Your password must include at least 2 symbols (`'\\~!@# $*%^&()<>,:;{}\|)'";
+        }
+
+        if (preg_match_all("/[0-9]/", $password) < 3) {
+            $validation['containsDigits']
+                = "Your password must include at least 3 digits";
+        }
+
+        if (strlen($password) <= 16) {
+            $validation['greaterThan16']
+                = "Your Password must be greater than 16";
+        }
+
+        if (!empty($validation)) {
+            return redirect('register')->with("password", $validation);
+        }
+
+        return $next($request);
+    }
+
+}
