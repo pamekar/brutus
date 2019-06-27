d881bb9166744a9c4b537bbd3430500b5024d5b8
GreenWhite <greenwhitedev@gmail.com>
2019-06-27 14:11:56 +0100

    Throttle Login attempts

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\LoginHacked;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -26,6 +33,10 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+    // maximum attempts for a failed login
+    protected $maxAttempts = 3;
+    // amount of minutes to elapse before logging in
+    protected $decayMinutes = 5;
 
     /**
      * Create a new controller instance.
@@ -36,4 +47,52 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  Request  $request
+     * @return RedirectResponse|Response|JsonResponse
+     *
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $this->sendEmailAlert($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  Request  $request
+     * @return void
+     *
+     */
+    protected function sendEmailAlert(Request $request){
+        Mail::to($request->email)->send(new LoginHacked());
+    }
+
 }
